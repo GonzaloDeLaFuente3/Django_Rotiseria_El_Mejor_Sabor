903ed0a94cae098044f970c25a14f722ddab50a6
Kevin Agüero <kevinalbertoaguero@gmail.com>
2022-10-31 12:57:55 -0300

    css arreglo

--- rotiseriaElMejorSabor/apps/empleado/admin.py
+++ rotiseriaElMejorSabor/apps/empleado/admin.py
@@ -6,11 +6,12 @@ class adminEmpleado(admin.ModelAdmin):
     #campos
     list_display = ["cuil", "nombre", "apellido","telefonoFijo","telefonoCelular","domicilio_calle","domicilio_numero","domicilio_departamento", "domicilio_localidad", "fechaIngreso"]
     #campos que se pueden modificar
-    list_editable = ["nombre", "apellido","domicilio_localidad", "domicilio_departamento","telefonoFijo","telefonoCelular","domicilio_calle","domicilio_numero"]
-    #porque campos se puede buscar
-    search_fields = ["cuil", "nombre","apellido"]
-    #filtros
-    list_filter = ["domicilio_localidad","domicilio_departamento"]
+    # list_editable = ["nombre", "apellido","domicilio_localidad", "domicilio_departamento","telefonoFijo","telefonoCelular","domicilio_calle","domicilio_numero"]
+    # #porque campos se puede buscar
+    # search_fields = ["cuil", "nombre","apellido"]
+    # #filtros
+    # list_filter = ["domicilio_localidad","domicilio_departamento"]
 
     list_per_page = 10
-admin.site.register(Empleado)
+
+admin.site.register(Empleado, adminEmpleado)
